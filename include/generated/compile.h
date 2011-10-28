7139f972d77922e7ce520d5daa55b8fae2ca9c0f
imoseyon <imoseyon@gmail.com>
2011-10-28 08:57:26 -0700

    die generated include files please

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 8 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 PREEMPT Fri Oct 28 08:00:43 PDT 2011"
-#define LINUX_COMPILE_TIME "08:00:43"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "corp.meebo.com"
-#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-50) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "2.6.35.14-imoseyon-5.0.0GBSalpha+"
