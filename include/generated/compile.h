9d13a309ba6440107af8e1f14558b9ccde0bbb53
imoseyon <imoseyon@gmail.com>
2011-11-08 06:56:45 -0800

    man i seriously fail at github - missing files readded

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 58 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#58 PREEMPT Thu Nov 3 05:27:32 PDT 2011"
+#define LINUX_COMPILE_TIME "05:27:32"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "corp.meebo.com"
+#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-50) "
