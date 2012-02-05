5570e3a6e1e036b8acb29f32eb3f8b153263ecb4
imoseyon <imoseyon@gmail.com>
2012-02-04 22:55:55 -0800

    cleanup - go away

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 5 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Sat Feb 4 20:47:47 PST 2012"
-#define LINUX_COMPILE_TIME "20:47:47"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "corp.meebo.com"
-#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-50) "
