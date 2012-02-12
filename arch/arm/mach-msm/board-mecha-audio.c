41839dd27b72428378ed8cd6e328b160401f54da
imoseyon <imoseyon@gmail.com>
2012-02-11 18:20:35 -0800

    took the headset and handset levels from express audio board

--- arch/arm/mach-msm/board-mecha-audio.c
+++ arch/arm/mach-msm/board-mecha-audio.c
@@ -45,16 +45,16 @@ void mecha_back_mic_enable(int);
 
 static struct q5v2_hw_info q5v2_audio_hw[Q5V2_HW_COUNT] = {
 	[Q5V2_HW_HANDSET] = {
-		.max_gain[VOC_NB_INDEX] = 0,
-		.min_gain[VOC_NB_INDEX] = 0,
-		.max_gain[VOC_WB_INDEX] = 0,
-		.min_gain[VOC_WB_INDEX] = 0,
+		.max_gain[VOC_NB_INDEX] = 400,
+		.min_gain[VOC_NB_INDEX] = -1600,
+		.max_gain[VOC_WB_INDEX] = 400,
+		.min_gain[VOC_WB_INDEX] = -1600,
 	},
 	[Q5V2_HW_HEADSET] = {
-		.max_gain[VOC_NB_INDEX] = 0,
-		.min_gain[VOC_NB_INDEX] = 0,
-		.max_gain[VOC_WB_INDEX] = 0,
-		.min_gain[VOC_WB_INDEX] = 0,
+		.max_gain[VOC_NB_INDEX] = 900,
+		.min_gain[VOC_NB_INDEX] = -1100,
+		.max_gain[VOC_WB_INDEX] = 900,
+		.min_gain[VOC_WB_INDEX] = -1100,
 	},
 	[Q5V2_HW_SPEAKER] = {
 		.max_gain[VOC_NB_INDEX] = 0,
